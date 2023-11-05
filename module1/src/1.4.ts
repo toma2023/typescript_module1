758bae3030ae5676e486fa0db936d63b30c4633f
toma2023 <mrinmoyehimadrita@gmail.com>
2023-11-05 20:18:36 +0600

    1.5ts done!

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -15,3 +15,13 @@ let y: null = null;
 let d : number;
 
 d = 123
+
+//Array
+let friends: string[] = ["toma", "mona"];
+friends.push("ruma");
+let eligibleRollList: number[] = [12, 15];
+
+//tuple ____>array______>order_____>type of values
+let coordinates : [number, number] = [12, 222]
+let ageName : [number, string, boolean] = [12, "TOMA", true]
+
---
+++ module1/src/1.5.ts
@@ -0,0 +1,13 @@
+//reference type >>> object
+
+const user: {
+    company: "Programming Hero";//literal type
+    firstName: string;
+    middleName?: string //optional type;
+    lastName: string
+} = {
+    company: "Programming Hero",
+    firstName: "Toma",
+    middleName: "Akter",
+    lastName: "Hashi"
+}
